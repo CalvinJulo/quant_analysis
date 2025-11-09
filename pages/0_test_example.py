97d8315b90688255c304ed74f94a5ab78d827445
Calvin Julo <cividis@hotmail.com>
2025-11-09 12:41:38 +0800

    Create 0_test_example.py

---
+++ pages/0_test_example.py
@@ -0,0 +1,23 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+"""
+# File       : xx.py
+# Time       ：2021/9/11 19:02
+# Author     ：
+# version    ：
+# Description：
+"""
+# CMD Run Command ： streamlit run /Users/xx.py --server.port 8501
+
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+import pandas as pd
+import streamlit as st
+
+st.write('Hello world')
