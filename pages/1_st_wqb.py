63cb18eb8c1cb37aa462762e76213d8b94b605ff
Calvin Julo <cividis@hotmail.com>
2025-11-09 23:17:44 +0800

    Update 1_st_wqb.py

--- pages/1_st_wqb.py
+++ pages/1_st_wqb.py
@@ -185,7 +185,7 @@ def simulate_alpha():
   if st.button('simulate'):
         for i in set_alpha_attr_dict:
           st.write(i)
-          alpha_setting_data = run_wqb.set_alpha(regular=i['regular'],region=i['region'],universe=i['universe'],
+          alpha_setting_data = run_wqb.set_alpha(regular=i['regular'],universe=i['universe'],
                                                  delay=i['delay'],decay=i['decay'],truncation=i['truncation'],neutralization=i['neutralization'])
           st.write(alpha_setting_data)
 
