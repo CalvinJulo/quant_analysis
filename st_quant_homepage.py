004f321b2aec6f361aeace8ccc60ee559348615e
Calvin Julo <cividis@hotmail.com>
2025-11-09 12:39:33 +0800

    Create st_quant_homepage.py

---
+++ st_quant_homepage.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+"""
+# File       : xx.py
+# Time       ：2021/9/11 19:02
+# Author     ：
+# version    ：python 3.9
+# Description：
+"""
+# CMD Run Command ： streamlit run /Users/xx.py --server.port 8501
+
+import streamlit as st
+
+st.write('## This is quant Case Homepage') 
